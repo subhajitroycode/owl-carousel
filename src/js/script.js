a114b1b0beeff6759a632372596f2bda0212cb36
Subhajit Roy <roysubhajit8697@gmail.com>
2022-05-29 01:11:35 +0530

    mobile responsive

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,19 @@
 var swiper = new Swiper(".mySwiper", {
-  slidesPerView: 3,
+  slidesPerView: 1,
   spaceBetween: 30,
-  slidesPerGroup: 3,
+  slidesPerGroup: 1,
   loop: true,
   loopFillGroupWithBlank: true,
+  breakpoints: {
+    450: {
+      slidesPerView: 2,
+      slidesPerGroup: 2,
+    },
+    900: {
+      slidesPerView: 3,
+      slidesPerGroup: 3,
+    },
+  },
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
